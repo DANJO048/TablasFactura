602f33d7f6031c1077e4327e8664d1e7c0f186bb
DANJO048 <42050651+DANJO048@users.noreply.github.com>
2018-10-20 17:42:58 -0600

    Initial commit

---
+++ src/Programa/Persona.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Programa;
+
+/**
+ *
+ * @author Josué Vásquez
+ */
+public class Persona {
+
+    String Nombre, Apellido, Edad;
+
+    public Persona(String Nombre, String Apellido, String Edad) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Edad = Edad;
+    }
+
+    Persona() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    
+}
